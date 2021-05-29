a93cffbf4dd8a3598c66e97b18c57ca685aacd30
enesvardar <41987216+enesvardar@users.noreply.github.com>
2021-05-29 23:47:11 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 17 21:06:09 2021
+
+@author: enesv
+"""
+"""
+                          mmmmmmmmmm          mmmmmmmmmm
+                        mmmmmmmmmmmmmm      mmmmmmmmmmmmmmmm
+                      mmmmmm    mmmmmmmm    mm        mmmmmm
+    mmmmmmmmmm        mmmmmm      mmmmmm              mmmmmm    mmmm          mmmm
+  mmmmmmmmmmmmmm      mmmmmm      mmmmmm              mmmmmm    mmmmmm        mmmm
+mmmmmm      mmmmmm    mmmmmm      mmmmmm        mmmmmmmmmm        mmmm      mmmm
+mmmmmmmmmmmmmmmmmm      mmmmmmmmmmmmmmmm        mmmmmmmmmmmm      mmmm      mmmm
+mmmmmmmmmmmmmmmmmm        mmmmmm  mmmmmm              mmmmmmmm    mmmmmm    mmmm
+mmmmmm                            mmmmmm                mmmmmm      mmmm  mmmm
+mmmmmmmm                        mmmmmm      mm        mmmmmmmm      mmmm  mmmm
+  mmmmmmmmmmmmmm        mmmmmmmmmmmm        mmmmmmmmmmmmmmmm          mmmmmm
+    mmmmmmmmmmmm        mmmmmmmmmm            mmmmmmmmmm              mmmmmm
+
+"""
+
+from game import *
+
+game = Game()
+
+game.initialDisplay() 
+
+game.loop() 
+
