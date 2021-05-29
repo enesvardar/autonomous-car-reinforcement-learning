a7a84cc62de5080ae590342f3186ca2641fc867a
enesvardar <41987216+enesvardar@users.noreply.github.com>
2021-05-29 23:40:25 +0300

    constants parameters

---
+++ defines.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May  4 23:56:44 2021
+
+@author: enesv
+"""
+
+TURN_THETA = 10 # turning angle of the car
+
+WINDOW_WIDTH = 600  # game window width
+WINDOW_HEIGHT = 400 # game window height
+
+BALL_START_POS_X = 300 # car x pos
+BALL_START_POS_Y = 200 # car y pos
+
+BALL_SPEED = 4 # car speed [pix]
+
+BALL_WIDTH = 10 # dimensions of the car object
+BALL_HEIGHT = 10
+
+OBSTACLE_WIDTH = 40 # dimensions of the obstacle object
+OBSTACLE_HEIGHT = 40
+
+OBSTACLE_NUMBER = 25 # number of obstacles
+
+ARM_SONAR_NUMBER = 5 # number of sensors of the car
+
+ARM_MAX_LENGHT = 100 # maximum distance sensing distance of the sensor
+
+BLACK = (0,0,0) # RGB
+RED = (255, 0, 0) 
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+GO_TURN_RIGHT = 0 # car actions
+GO_TURN_LEFT = 1
+GO_STRAIGHT = 2
+
